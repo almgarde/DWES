4cee09d504727a143537c0cf7a51bcfc0e9081c0
almgarde <72916143+almgarde@users.noreply.github.com>
2021-09-22 10:55:28 +0200

    Add files via upload

---
+++ Nomina1/src/main/java/Laboral/CalculaNominas.java
@@ -0,0 +1,56 @@
+package Laboral;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalculaNominas {
+	
+	/**
+	 * Logger
+	 */
+	final static Logger LOGGER = LoggerFactory.getLogger(CalculaNominas.class);
+
+    /**
+     * Método principal
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+    	
+    	LOGGER.info("Inicio del método principal");
+
+        try {
+
+            //Creación de objetos empleados y nómina
+            Empleado emp1 = new Empleado(4, 7, "James Cosling", "32000032G", 'M');
+
+            Empleado emp2 = new Empleado("Ada Lovelace", "32000031R", 'F');
+
+            Nomina nom = new Nomina();
+
+            // Impresión de los atributos y sueldo de los empleados
+            escribe(emp1, emp2, nom);
+
+            // Incremento de años para el empleado 2
+            emp2.incrAnyo();
+
+            // Establecimiento de la categoría del empleado 1
+            emp1.setCategoria(9);
+
+            // Impresión de los atributos y sueldo de los empleados
+            escribe(emp1, emp2, nom);
+        } catch (DatosNoCorrectosException e) {
+            System.out.println("Datos no correctos");
+        }
+
+        LOGGER.info("Fin del método principal");
+    }
+
+    // Método de impresión de atributos y sueldo de los empleados
+    private static void escribe(Empleado emp1, Empleado emp2, Nomina nom) {
+        emp1.imprime();
+        System.out.println("Sueldo del empleado 1: " + nom.sueldo(emp1));
+        emp2.imprime();
+        System.out.println("Sueldo del empleado 2: " + nom.sueldo(emp2));
+    }
+}
