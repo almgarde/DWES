939cdfb79210c291bc8ee08c69b00902cd149ca3
almgarde <72916143+almgarde@users.noreply.github.com>
2021-09-26 18:31:38 +0200

    Add files via upload

---
+++ Nomina2.DB/src/main/java/Laboral/DBUtils.java
@@ -0,0 +1,26 @@
+package Laboral;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBUtils {
+		
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+		String url = "jdbc:oracle:thin:@192.168.1.135:1521/XE";
+		String user = "dwes";
+		String pass = "dwes";        
+
+        conn = DriverManager.getConnection(url, user, pass);
+        return conn;
+
+    }
+    
+    public static void close(Connection conn) throws SQLException {
+        if (conn != null)
+            conn.close();
+    }
+
+}
