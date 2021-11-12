442787d717e5fd97c2aea5886c91d466c4275942
almgarde <72916143+almgarde@users.noreply.github.com>
2021-11-12 23:27:53 +0100

    Add files via upload

---
+++ NominaSpringBoot/src/test/java/dwes/nomina/NominaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dwes.nomina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
